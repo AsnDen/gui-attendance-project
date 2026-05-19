73efea8953ff768a1b9652ba484cc810e0841483
Eropg-oss <kutcepaloverop@gmail.com>
2026-05-19 18:12:36 +0300

    Fixing conflicts

--- src/roll/services/__init__.py
+++ src/roll/services/__init__.py
@@ -1,3 +1,10 @@
+from roll.services.exceptions import EmptyLabelError, PersonNotFoundError
+from roll.services.person_service import PersonService
 from roll.services.qr_reader_service import QRIdentifierReaderService
 
-__all__ = ["QRIdentifierReaderService"]
+__all__ = [
+    "EmptyLabelError",
+    "PersonNotFoundError",
+    "PersonService",
+    "QRIdentifierReaderService",
+]
