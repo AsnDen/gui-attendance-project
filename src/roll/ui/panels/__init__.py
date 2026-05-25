c95db92715c3fc2c44258a8e62c31307c3b6d2a7
AsnDen <asnden@gmail.com>
2026-05-26 01:18:56 +0300

    fix(ui): fixed import file name

--- src/roll/ui/panels/__init__.py
+++ src/roll/ui/panels/__init__.py
@@ -1,5 +1,5 @@
 from roll.ui.panels.calendar_panel import CalendarPanel
-from roll.ui.panels.day_event_panel import DayEventsPanel
+from roll.ui.panels.day_events_panel import DayEventsPanel
 from roll.ui.panels.scanner_panel import ScannerPanel
 
 __all__ = ["CalendarPanel", "DayEventsPanel", "ScannerPanel"]
